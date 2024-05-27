f32aed9a16818769eb95a23678b1dd0c0e4dac7b
Enzo <enzosamudio@gmail.com>
2024-05-27 12:00:58 -0300

    cambio imagen slider

--- src/app/home/components/slider/slider.component.ts
+++ src/app/home/components/slider/slider.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css'
 })
