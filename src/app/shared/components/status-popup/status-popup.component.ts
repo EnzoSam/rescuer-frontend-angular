5f5cc17a65f327846778579764de7e8447b71184
Enzo <enzosamudio@gmail.com>
2024-05-28 12:03:16 -0300

    cambio en estilos

--- src/app/shared/components/status-popup/status-popup.component.ts
+++ src/app/shared/components/status-popup/status-popup.component.ts
@@ -29,7 +29,7 @@ export class StatusPopupComponent implements OnInit, OnDestroy {
 
         this.show();        
         this.status = _status;
-        
+        console.log(_status)
         this.intervalSubscription = interval(3000)
           .subscribe(x => { this.hide(); });
       },
