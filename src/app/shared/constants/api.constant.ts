f2d42d2f5d0ec429a440a76005cb5f4954260bfb
Enzo <enzosamudio@gmail.com>
2024-06-18 17:36:36 -0300

    prod

--- src/app/shared/constants/api.constant.ts
+++ src/app/shared/constants/api.constant.ts
@@ -1,6 +1,15 @@
 export const Backend =
 {
+    BaseUrl : 'https://rescuers-api-ts.vercel.app/api/',
+    UploadsUrl : 'https://rescuers-api-ts.vercel.app/api/',
+    ResourcesUrl:'https://storage.googleapis.com/rescuers-e684f.appspot.com/'
+}
+
+/*
+export const Backend =
+{
     BaseUrl : 'http://localhost:3990/api/',
     UploadsUrl : 'http://localhost:3990/api/',
     ResourcesUrl:'https://storage.googleapis.com/rescuers-e684f.appspot.com/'
 }
+*/
